8aeb6bf8d0135f31768cc68d4432cc188977841e
KayGV25 <khuongvudang25@gmail.com>
2023-11-18 11:15:56 +0700

    Complete FE main page

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,21 @@
+// Search db for movies -> return none page if none is found
 function Search(){
     let value = document.getElementById("search-content").value;
     alert(value);
 }
+// function to activate side menu
 function sidebar(){
     var bg = document.getElementById("dark-bg");
     var sidebar = document.getElementById("side-menu");
     bg.classList.toggle("hide");
     sidebar.classList.toggle("hide");
 }
-function ShowContent(){
-    var drop = document.getElementById("drop");
+// general function to show ddrop down content (not show on hover)
+function ShowContent(id){
+    var drop = document.getElementById(id);
     drop.classList.toggle("drop");
 }
-
+//Press enter to search
 document.addEventListener("keydown",function(key){
     if(document.getElementById("search-content").value != ''){
         if(key.key == "Enter"){
