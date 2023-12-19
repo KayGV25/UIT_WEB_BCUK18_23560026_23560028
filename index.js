7a6f50c06b6b65026b8a3b9d5835cedab16f1830
kiiiiiiiiif <kyle.vuanh@gmail.com>
2023-12-19 08:30:44 +0700

    ok
    ok

---
+++ index.js
@@ -0,0 +1,112 @@
+// Import essential libraries 
+const { log } = require('console');
+const express = require('express'); 
+const { link } = require('fs');
+const app = express(); 
+const path = require('path'); 
+const { nextTick } = require('process');
+const router = express.Router();
+var MomoLink = '';
+// Setup essential routes 
+router.get('/', function(req, res) { 
+    res.render(path.join(__dirname + '/views/index.pug')); 
+}); 
+router.get('/index', function(req, res) { 
+    res.render(path.join(__dirname + '/views/index.pug')); 
+    //__dirname : It will resolve to your project folder. 
+}); 
+router.get('/film', function(req, res) { 
+    res.render(path.join(__dirname + '/views/film.pug')); 
+});
+router.get('/login', function(req, res) { 
+    res.render(path.join(__dirname + '/views/login.pug')); 
+}); 
+router.get('/search', function(req, res) { 
+    res.render(path.join(__dirname + '/views/search.pug')); 
+}); 
+router.get('/upgrade-plan', function(req, res) { 
+    res.render(path.join(__dirname + '/views/upgrade-plan.pug')); 
+});router.post('/checkout', async function(req,res){
+    var partnerCode = "MOMO";
+    var accessKey = "F8BBA842ECF85";
+    var secretkey = "K951B6PE1waDMi640xX08PD3vg6EkVlz";
+    var requestId = partnerCode + new Date().getTime();
+    var orderId = requestId;
+    var orderInfo = "pay with MoMo";
+    var redirectUrl = "http://localhost:8000/index";
+    var ipnUrl = "http://localhost:8000/index";
+    // var ipnUrl = redirectUrl = "https://webhook.site/454e7b77-f177-4ece-8236-ddf1c26ba7f8";
+    var amount = "70000";
+    var requestType = "payWithATM"
+    var extraData = ""
+
+    // Generate raw signature
+    var rawSignature = "accessKey="+accessKey+"&amount=" + amount+"&extraData=" + extraData+"&ipnUrl=" + ipnUrl+"&orderId=" + orderId+"&orderInfo=" + orderInfo+"&partnerCode=" + partnerCode +"&redirectUrl=" + redirectUrl+"&requestId=" + requestId+"&requestType=" + requestType        //puts raw signature
+    console.log("--------------------RAW SIGNATURE----------------")
+    console.log(rawSignature)
+    //signature
+    const crypto = require('node:crypto');
+    var signature = crypto.createHmac('sha256', secretkey)
+        .update(rawSignature)
+        .digest('hex');
+    console.log("--------------------SIGNATURE----------------")
+    console.log(signature)
+
+    // Construct request body
+    const requestBody = JSON.stringify({
+        partnerCode : partnerCode,
+        accessKey : accessKey,
+        requestId : requestId,
+        amount : amount,
+        orderId : orderId,
+        orderInfo : orderInfo,
+        redirectUrl : redirectUrl,
+        ipnUrl : ipnUrl,
+        extraData : extraData,
+        requestType : requestType,
+        signature : signature,
+        lang: 'en'
+    });
+
+    // Create HTTPS request object
+    const https = require('https');
+    const options = {
+        hostname: 'test-payment.momo.vn',
+        port: 443,
+        path: '/v2/gateway/api/create',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(requestBody)
+        }
+    };
+
+    const reqd = https.request(options, res => {
+        console.log(`Status: ${res.statusCode}`);
+        console.log(`Headers: ${JSON.stringify(res.headers)}`);
+        res.setEncoding('utf8');
+        res.on('data', (body) => {
+            console.log('Body: ');
+            console.log(body);
+            console.log('payUrl: ');
+            console.log(JSON.parse(body));
+            MomoLink = JSON.parse(body).payUrl;
+        });
+        res.on('end', () => {
+            console.log('No more data in response.');
+        });
+    })
+    console.log("Sending....")
+    reqd.write(requestBody);
+    reqd.end();
+    setTimeout(() => {
+    console.log('Mm: ' + MomoLink);
+    res.redirect(MomoLink);
+    }, 5000);
+})
+//add the router 
+app.use('/public/stylesheets',express.static(__dirname +'/public/stylesheets'));
+app.use('/routes',express.static(__dirname +'/routes'));
+app.use('/', router);
+app.listen(process.env.port || 8000); 
+console.log('Running at Port 8000');
---
+++ routes/search.js
@@ -0,0 +1,88 @@
+const search_bar_url = window.location.search;
+const param = new URLSearchParams(search_bar_url);
+
+const type = param.get('type');
+const name = param.get('name');
+console.log(name);
+
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYzFjNjc3MTI5Y2I1ZGQxNzM4OTRmNzQyZmNjNmMwZCIsInN1YiI6IjY1NWMzNjg4ZDRmZTA0MDBjNDI1N2QzYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XM1rkMipIifqel9k6Q1xruUwaYDD7oy2g8ylt9kgkTw'
+    }
+  };
+  
+if(type == "name"){
+    fetch(`https://api.themoviedb.org/3/search/multi?query=${name}&include_adult=false&language=en-US&page=1`, options)
+    .then(response => response.json())
+    .then(response => {
+        ShowMovies(response);
+    })
+    .catch(err => console.error(err));
+}
+if(type == "genre"){
+    fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=${name}`, options)
+    .then(response => response.json())
+    .then(response => {
+        ShowMovies(response);
+    })
+    .catch(err => console.error(err));
+}
+
+
+//Press enter to search
+document.addEventListener("keydown",function(key){
+    if(document.getElementById("search-content").value != ''){
+        if(key.key == "Enter"){
+            Search();
+        }
+    }
+})
+// Search db for movies -> return none page if none is found
+function Search(){
+    let value = document.getElementById("search-content").value;
+    window.location.replace(`search?name=${value}&type=name`)
+}
+function SearchByGenre(id){
+    // let value = document.getElementById("search-content").value;
+    window.location.replace(`search?name=${id}&type=genre`);
+}
+// function to activate side menu
+function sidebar(){
+    var bg = document.getElementById("dark-bg");
+    var sidebar = document.getElementById("side-menu");
+    bg.classList.toggle("hide");
+    sidebar.classList.toggle("hide");
+}
+function ShowContent(id){
+    var drop = document.getElementById(id);
+    drop.classList.toggle("drop");
+}
+function ShowMovies(response){
+    for(i in response.results) console.log(response.results[i].first_air_date)
+    ft_movies = response.results;
+    let ft_movies_content = ''
+    for(ele in ft_movies){
+        if(ft_movies[ele].poster_path != null){
+            title = ft_movies[ele].title;
+            date = ft_movies[ele].release_date;
+            if(ft_movies[ele].media_type == "tv"){
+                title = ft_movies[ele].name;
+                date = ft_movies[ele].first_air_date;
+            }
+            ft_movies_content += `
+            <div class="film" id="${ft_movies[ele].id}">
+                <a href="film?id=${ft_movies[ele].id}">
+                    <div class="thumbnail" style="background-image: url('https://image.tmdb.org/t/p/w500${ft_movies[ele].poster_path}');background-size: contain;"></div>
+                    <div class="film-des-show">
+                        <p class="title">${title}</p>
+                        <div class="other">${date}</div>
+                    </div>
+                </a>
+            </div>`;
+        }
+    }
+    document.getElementById("main").innerHTML = ft_movies_content
+    console.log(ft_movies);
+}
