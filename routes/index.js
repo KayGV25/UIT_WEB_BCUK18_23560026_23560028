28c1aa48efa86ac7ff6492f29fec1987ca8f3177
KayGV25 <khuongvudang25@gmail.com>
2023-12-23 02:03:41 +0700

    add redirect to login

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get(['/', '/index'], /*checkLogin,*/ function(req, res, next)  {
-  res.render('index');
+  if(req.session.loggedIn) res.render('index')
+  res.redirect('/login');
 });
 /* GET film page. */
 router.get('/film', function(req, res, next) {
