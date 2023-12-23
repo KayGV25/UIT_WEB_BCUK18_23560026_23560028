e7334057b5c6287fdb5696fb395dd36671e5d706
kiiiiiiiiif <kyle.vuanh@gmail.com>
2023-12-23 09:05:11 +0700

    add login block on all GET

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,25 @@
 var express = require('express');
 var router = express.Router();
-// var { checkLogin } = require('../routes/authentication'); related to block login
 
 /* GET home page. */
-router.get(['/', '/index'], /*checkLogin,*/ function(req, res, next)  {
+router.get(['/', '/index'], function(req, res, next)  {
   if(req.session.loggedIn) res.render('index')
   res.redirect('/login');
 });
 /* GET film page. */
 router.get('/film', function(req, res, next) {
-  res.render('film');
+  if(req.session.loggedIn) res.render('film')
+  res.redirect('/login');
 });
 /* GET search. */
 router.get('/search', function(req, res, next) {
-  res.render('search');
+  if(req.session.loggedIn) res.render('search')
+  res.redirect('/login');
 });
 /* GET upgrade-plan page. */
 router.get('/upgrade-plan', function(req, res, next) {
-  res.render('upgrade-plan');
+  if(req.session.loggedIn) res.render('upgrade-plan')
+  res.redirect('/login');
 });
 
 module.exports = router;
